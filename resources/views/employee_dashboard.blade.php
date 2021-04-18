dda9b688581f021096dbdf1bb9aa0de6e7dc9b9f
Taiwonator <taiwonator77@gmail.com>
2021-04-18 17:27:15 +0100

    added main content

--- resources/views/employee_dashboard.blade.php
+++ resources/views/employee_dashboard.blade.php
@@ -33,6 +33,25 @@
         color: inherit;
     }
 
+    button {
+        color: #161616;
+        background: #03fcb1;
+        outline: none;
+        border: none;
+        padding: .5em 1em;
+        border-radius: 2.5px;
+    }
+
+    input, select {
+        background: #161616;
+        color: grey;
+        outline: none;
+        border: none;
+        border-radius: 2.5px;
+        padding: .5em 1.5em;
+        min-width: 40%;
+    }
+
     .wrapper {
         display: grid;
         grid-template-areas:
@@ -107,6 +126,96 @@
 
     #main-content {
         grid-area: main-content;
+        padding: 0 3vw 0 0;
+        flex-direction: column;
+    }
+
+    .content-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-bottom: 1em;
+    }
+
+    .top-row h2 {
+        text-transform: uppercase;
+    }
+
+    .content-row .stat-box {
+        display: flex;
+        background: #1C1C1C;
+        border-radius: 5px;
+        padding: 2em;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .stat-box p {
+        margin-bottom: 1em;
+    }
+
+    .stat-box h3 {
+        font-size: 50px;
+    }
+
+    .content-row .content-box {
+        display: flex;
+        background: #1C1C1C;
+        border-radius: 5px;
+        padding: 2em;
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .content-box .top-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-bottom: 2em;
+    }
+
+    .content-box .content-box-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-bottom: 2em;
+    }
+
+    .content-box-row p {
+        font-weight: bold;
+    }
+    
+    .content-form {
+        width: 100%;
+    }
+
+    .content-form .form-row {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-bottom: 1em;
+    }
+
+    .form-row .form-input {
+        display: flex;
+        flex-direction: column;
+        min-width: 40%;
+    }
+
+    .textarea-input {
+        width: 100%;
+    }
+
+    .textarea-input input {
+        height: 100px;
+    }
+
+    .form-input label {
+        font-size: 14px;
+        margin-bottom: .5em;
+        /* font-weight: 600; */
+        /* color: #03fcb1; */
+        /* text-transform: uppercase; */
     }
 
     @media (max-width: 1200px) {
@@ -158,6 +267,95 @@
             </div>
         </nav>
         <div id="main-content">
+            <div class="content-row">
+
+                <div class="stat-box">
+                    <p>Calls made:</p>
+                    <h3>6</h3>
+                </div>
+
+                <div class="stat-box">
+                    <p>Unsolved tickets:</p>
+                    <h3>2</h3>
+                </div>
+
+            </div>
+
+            <div class="content-row">
+
+                <div class="content-box">
+                    <div class="top-row">
+                        <h2>New Ticket</h2>
+                        <button>Submit ticket</button>
+                    </div>
+
+                    <div class="content-box-row">
+                        <p>Please input problem details:</p>
+                    </div>
+                    <div class="content-box-row">
+                        <form class="content-form" action="">
+                            <div class="form-row">
+                                <div class="form-input date-input">
+                                    <label>Date of problem</label>
+                                    <input type="date">
+                                </div>
+                                <div class="form-input time-input">
+                                    <label>Date of problem</label>
+                                    <input type="time">
+                                </div>
+                            </div>  
+                            <div class="form-row">
+                                <div class="form-input textarea-input">
+                                    <label>Description of problem</label>
+                                    <input type="textarea">
+                                </div>
+                            </div>  
+                            <div class="form-row">
+                                <div class="form-input dropdown-input">
+                                    <label>Software used</label>
+                                    <select name="" id="">
+                                        <option value="" disabled selected>Select your software</option>
+                                        <option value="">Software 1</option>
+                                        <option value="">Software 2</option>
+                                        <option value="">Software 3</option>
+                                    </select>
+                                </div>
+                                <div class="form-input dropdown-input">
+                                    <label>Operating system</label>
+                                    <select name="" id="">
+                                        <option value="" disabled selected>Select your OS</option>
+                                        <option value="">OS 1</option>
+                                        <option value="">OS 2</option>
+                                        <option value="">OS 3</option>
+                                    </select>
+                                </div>
+                            </div>  
+                            <div class="form-row">
+                                <div class="form-input dropdown-input">
+                                    <label>Problem type</label>
+                                    <select name="" id="">
+                                        <option value="" disabled selected>Select your problem type</option>
+                                        <option value="">Problem Type 1</option>
+                                        <option value="">Problem Type 2</option>
+                                        <option value="">Problem Type 3</option>
+                                    </select>
+                                </div>
+                                <div class="form-input dropdown-input">
+                                    <label>Priority</label>
+                                    <select name="" id="">
+                                        <option value="" disabled selected>Select your priority</option>
+                                        <option value="">Priority 1</option>
+                                        <option value="">Priority 2</option>
+                                        <option value="">Priority 3</option>
+                                    </select>
+                                </div>
+                            </div>    
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+            
         
         </div>
     </div>
