9ec2a4e90e3026b8ca8d6e99b4120d350175f2df
JamesThanni <31824913+JamesThanni@users.noreply.github.com>
2021-05-09 17:40:10 +0100

    Add files via upload
    + Created a call logs page using our application of the Laravel Blade system, where the Specialists' logged calls can be displayed with default or selected sorting methods

---
+++ resources/views/specialist_logbook.blade.php
@@ -0,0 +1,77 @@
+
+        @extends('specialist_page')
+
+        @section('specialist-page-content')
+            
+        {{-- <nav id="side-nav">
+            <div class="side-nav-item">
+                <i class="fa fa-list"></i>
+                <h3>Sort by Most Recent</h3>
+            </div>
+            <div class="side-nav-item">
+                <i class="fa fa-list"></i>
+                <h3>Sort by Caller</h3>
+            </div>
+            
+        </nav> --}}
+        <div id="main-content" class="scroll">
+            <div class="content-row" id="divided-content">
+                </div class="top-row">
+                    Recent Calls
+                </div>
+
+                <div class="content-box-row">
+                    <strong>17:38</strong>
+                    <p>
+                        Microsoft word has been closing unexpectedly.
+                    </p>
+
+                    <p class="caller">
+                        Jeff Besos
+                    </p>
+                </div>
+                <div class="content-box-row">
+                    <strong>17:38</strong>
+                    <p>
+                        Microsoft word has been closing unexpectedly.
+                    </p>
+
+                    <p class="caller">
+                        Jeff Besos
+                    </p>
+                </div>
+                <div class="content-box-row">
+                    <strong>17:38</strong>
+                    <p>
+                        Microsoft word has been closing unexpectedly.
+                    </p>
+
+                    <p class="caller">
+                        Jeff Besos
+                    </p>
+                </div>
+                <div class="content-box-row">
+                    <strong>17:38</strong>
+                    <p>
+                        Microsoft word has been closing unexpectedly.
+                    </p>
+
+                    <p class="caller">
+                        Jeff Besos
+                    </p>
+                </div>
+                <div class="content-box-row">
+                    <strong>17:38</strong>
+                    <p>
+                        Microsoft word has been closing unexpectedly.
+                    </p>
+
+                    <p class="caller">
+                        Jeff Besos
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        
+        @endsection
