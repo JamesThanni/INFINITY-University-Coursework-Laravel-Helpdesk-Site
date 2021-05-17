fd12b445067f253c8af99c59bddf82bfa69dc51f
JamesThanni <31824913+JamesThanni@users.noreply.github.com>
2021-05-17 14:47:22 +0100

    Update default.blade.php

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <title>Employee Dashboard</title>
+    <title>Make-It-All Helpdesk</title>
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap');
      /* https://codepen.io/GhostRider/pen/GHaFw */
