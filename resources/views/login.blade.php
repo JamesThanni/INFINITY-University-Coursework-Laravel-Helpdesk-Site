a2f436571fbb4188a7adf0a7a47ff64eae5a1ea7
JamesThanni <31824913+JamesThanni@users.noreply.github.com>
2021-05-17 13:53:16 +0100

    Add files via upload
    + Updated login form to use the login validation function for empty inputs
    + Removed testing information for user accounts

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,10 +6,10 @@
         <div class="login-box-container">
                 <i class="fa fa-wrench blue fa-3x"></i>
                 <h1>Make-It-All-Helpdesk</h1>
-                <form method="post" action="login/user">
+                <form method="post" action="login/user" onsubmit="return loginValidation();">
                         @csrf <!-- {{ csrf_field() }} -->
-                        <input type="text" name="email" placeholder="Email">
-                        <input type="text" name="password" placeholder="Password">
+                        <input id="email" type="text" name="email" placeholder="Email">
+                        <input id="password" type="text" name="password" placeholder="Password">
                         <button type="submit">Submit</button>
                 </form>
 
@@ -18,16 +18,15 @@
                         {{-- <a href="{{ url('employee/dashboard') }}"><h3 class="nav-item">Employee</h3></a>
                         <a href="{{ url('analyst/dashboard') }}"><h3 class="nav-item">Analyst</h3></a>
                         <a href="{{ url('specialist/dashboard') }}"><h3 class="nav-item">Specialist</h3></a> --}}
+
+                        <!--
                         <p>Analyst: J.Bob@makeitall.com - 1234567</p>
                         <p>Employee: M.Smith@makeitall.com - 1234567</p>
                         <p>Specialist: J.Doe@makeitall.com - 1234567</p>
+                        -->
                 </div>
-
-                @foreach( $users as $user ) 
-                        <p>{{ $user->userType }}</p>
-                        <p>{{ $user->email }}</p>
-                        <p>{{ $user->password }}</p>
-                @endforeach
+                
+               
         </div>
 
         @endsection
